c2082e144522cac835cc2df1592a5d77837d864b
matiasfelau <matiasfelau@gmail.com>
2024-04-04 10:28:30 -0300

    abriendo repo

---
+++ app/src/main/java/ar/edu/uade/tateti/activities/GameActivity.java
@@ -0,0 +1,4 @@
+package ar.edu.uade.tateti.activities;
+
+public class GameActivity {
+}
---
+++ app/src/main/java/ar/edu/uade/tateti/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package ar.edu.uade.tateti.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ar.edu.uade.tateti.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
