0f0ba98d538f2fa74a1d19ca71a290dcafdf2250
Ariana <arifbottaro@gmail.com>
2024-04-05 15:36:48 -0300

    controldejuego

---
+++ app/src/main/java/ar/edu/uade/tateti/ControlDeJuego.java
@@ -0,0 +1,16 @@
+package ar.edu.uade.tateti;
+
+public class ControlDeJuego {
+
+    public void asignarValorJugado(){
+
+    }
+
+    public boolean gano(){
+
+    }
+
+    public void proximoMovimiento(){
+
+    }
+}
